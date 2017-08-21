e5fb62e65e751858b476305850a08d30c0eb4ab4
Keano Lane <keanolane@gmail.com>
2017-08-21 13:44:45 +0100

    Split SCRUM report by list (#13)

--- cjs/scrum-report-tool.js
+++ cjs/scrum-report-tool.js
@@ -61,20 +61,21 @@ class Member {
 }
 
 /**
- * @typedef {Object} Report
+ * @typedef {Object} List
  * @property {number} cards - The total amount of cards done for this sprint.
  * @property {number} points - The total amount of points done for this sprint.
  * @property {Member[]} members - The stats split by member.
  */
-class Report {
+class List {
     /**
      * CardItem constructor.
      *
      * @param props
      */
     constructor(props) {
-        this.cards = null;
-        this.points = null;
+        this.name = null;
+        this.cards = 0;
+        this.points = 0;
         this.members = [];
 
         this.assign(props);
@@ -87,6 +88,10 @@ class Report {
      */
     assign(props) {
         if (props) {
+            if (props.name) {
+                this.name = props.name;
+            }
+
             if (props.cards) {
                 this.cards = props.cards;
             }
@@ -125,6 +130,88 @@ class Report {
     addMember(member) {
         this.members.push(member);
     }
+}
+
+/**
+ * @typedef {Object} Report
+ * @property {number} cards - The total amount of cards done for this sprint.
+ * @property {number} points - The total amount of points done for this sprint.
+ * @property {List[]} lists - The stats split by list.
+ * @property {Member[]} members - The stats split by member.
+ */
+class Report {
+    /**
+     * CardItem constructor.
+     *
+     * @param props
+     */
+    constructor(props) {
+        this.cards = 0;
+        this.points = 0;
+        this.lists = [];
+        this.members = [];
+
+        this.assign(props);
+    }
+
+    /**
+     * Assigns properties.
+     *
+     * @param {object} props
+     */
+    assign(props) {
+        if (props) {
+            if (props.cards) {
+                this.cards = props.cards;
+            }
+
+            if (props.points) {
+                this.points = props.points;
+            }
+
+            if (props.lists) {
+                this.lists = props.lists;
+            }
+
+            if (props.members) {
+                this.members = props.members;
+            }
+        }
+    }
+
+    /**
+     * Increments the cards counter.
+     */
+    addCard() {
+        this.cards++;
+    }
+
+    /**
+     * Increments the points counter with the specified value.
+     *
+     * @param {number} points
+     */
+    addPoints(points) {
+        this.points += points;
+    }
+
+    /**
+     * Adds a List to the lists list.
+     *
+     * @param {List} list
+     */
+    addList(list) {
+        this.lists.push(list);
+    }
+
+    /**
+     * Adds a Member to the members list.
+     *
+     * @param {Member} member
+     */
+    addMember(member) {
+        this.members.push(member);
+    }
 
     /**
      * Returns the console styling for titles.
@@ -145,6 +232,15 @@ class Report {
     }
 
     /**
+     * Returns the console styling for headers.
+     *
+     * @returns {string}
+     */
+    getHeaderStyle() {
+        return 'font-size: 14px; font-weight: bold; line-height: 18px; color: #262626;';
+    }
+
+    /**
      * Returns the console styling for normal text.
      *
      * @returns {string}
@@ -157,20 +253,40 @@ class Report {
      * Prints the report details in the console.
      */
     printReport() {
+        console.log('===================================');
         console.log('%c SCRUM REPORT FOR CURRENT SPRINT ', this.getTitleStyle());
+        console.log('===================================');
 
         console.log('%c Global ', this.getSubTitleStyle());
         console.log('%c Cards: ' + this.cards, this.getNormalTextStyle());
         console.log('%c Points: ' + this.points, this.getNormalTextStyle());
+        console.log('%c Lists: ' + this.lists.length, this.getNormalTextStyle());
         console.log('%c Members: ' + this.members.length, this.getNormalTextStyle());
 
         var reportClass = this;
 
-        $.each(this.members, function(index, member) {
-            console.log('%c ' + member.name + ' ', reportClass.getSubTitleStyle());
-            console.log('%c Cards: ' + member.cards, reportClass.getNormalTextStyle());
-            console.log('%c Points: ' + member.points, reportClass.getNormalTextStyle());
-        });
+        if (this.lists.length === 0) {
+            console.error('No lists in report');
+        } else {
+            $.each(this.lists, function(index, list) {
+                console.log('%c ' + list.name + ' ', reportClass.getSubTitleStyle());
+                console.log('%c Cards: ' + list.cards, reportClass.getNormalTextStyle());
+                console.log('%c Points: ' + list.points, reportClass.getNormalTextStyle());
+
+                if (list.members.length === 0) {
+                    console.error('No members in list');
+                } else {
+                    $.each(list.members, function (subIndex, member) {
+                        console.log('%c ' + member.name + ' ', reportClass.getHeaderStyle());
+                        console.log('%c Cards: ' + member.cards, reportClass.getNormalTextStyle());
+                        console.log('%c Points: ' + member.points, reportClass.getNormalTextStyle());
+                    });
+                }
+            });
+        }
+        console.log('===============');
+        console.log('===== END =====');
+        console.log('===============');
     }
 }
 
@@ -185,56 +301,58 @@ var cardSelector = '.list-card';
 var cardScrummerPointsSelector = '.scrummer-points';
 var cardMemberAvatarSelector = '.member-avatar';
 
-var startList = 'QA';
-
 /**
  * Builds and prints the Scrum Report for current board.
  */
 function getScrumReport() {
     /** @type {Report} */
     var report = new Report();
-    var doneLists = getDoneLists();
-    var members = [];
+    var allLists = $(listSelector);
 
-    $.each(doneLists, function(index, $list) {
+    $.each(allLists, function(index, item) {
+        var $list = $(item);
+        var listName = $($list.find(listTitleSelector)).text();
         var listPoints = getTotalPointsForList($list);
 
-        processCardsInList(report, members, $list);
+        var list = new List({
+            name: listName,
+            points: listPoints
+        });
 
+        processCardsInList(list, $list);
+
+        report.addList(list);
         report.addPoints(listPoints);
+
+        for (var i=0; i<list.cards; i++) {
+            report.addCard();
+        }
     });
 
-    for (var index in members) {
-        report.addMember(members[index]);
+    for (var i in report.lists) {
+        var list = report.lists[i];
+
+        for (var j in list.members) {
+            var member = list.members[j];
+
+            if (report.lists.findIndex(findMember, member) === -1) {
+                report.addMember(member);
+            }
+        }
     }
 
     report.printReport();
 }
 
 /**
- * Returns the list of Trello lists needed for Scrum Report.
+ * Filter callback to find a member in a members list.
  *
- * @returns {jQuery[]}
+ * @param {Member} item
+ * @param {Member} member
+ * @returns {bool}
  */
-function getDoneLists() {
-    var $lists = $(listSelector);
-    var firstDoneListReached = false;
-    var doneLists = [];
-
-    $.each($lists, function(index, list) {
-        var $list = $(list);
-        var $title = $($list.find(listTitleSelector));
-
-        if ($title.text() == startList) {
-            firstDoneListReached = true;
-        }
-
-        if (firstDoneListReached) {
-            doneLists.push($list);
-        }
-    });
-
-    return doneLists;
+function findMember(item, member) {
+    return item.name === member.name;
 }
 
 /**
@@ -261,12 +379,14 @@ function getTotalPointsForList($list) {
 /**
  * Process cards data in specified list.
  *
- * @param {Report} report
- * @param {object[]} members
+ * @param {List} list
  * @param {jQuery} $list
  */
-function processCardsInList(report, members, $list) {
+function processCardsInList(list, $list) {
+    console.info('Processing cards in list "' + list.name + '"');
+
     var $cards = $($list.find(cardSelector));
+    var members = [];
 
     $.each($cards, function(index, card) {
         var $card = $(card);
@@ -287,8 +407,12 @@ function processCardsInList(report, members, $list) {
             processMembers(members, $cardMembers, pointsPerMember);
         }
 
-        report.addCard();
+        list.addCard();
     });
+
+    for (var i in members) {
+        list.addMember(members[i]);
+    }
 }
 
 /**
@@ -302,7 +426,9 @@ function calcPointsPerMember($cardPoints, $cardMembers) {
     var cardPoints = parseInt($cardPoints.text());
     var cardMembersCount = $cardMembers.length;
 
-    return (cardMembersCount) ? cardPoints / cardMembersCount : cardPoints;
+    var points = (cardMembersCount) ? cardPoints / cardMembersCount : cardPoints;
+
+    return Math.ceil(points);
 }
 
 /**
